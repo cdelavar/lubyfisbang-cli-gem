97e3c8cc4399a2ee5934fbfaf1f8b8c03dd6c857
jinjo39 <c.delavar@gmail.com>
2016-04-05 03:45:38 -0700

    added case statement

--- lib/lubyfisbang/cli.rb
+++ lib/lubyfisbang/cli.rb
@@ -13,22 +13,31 @@ class Lubyfisbang::CLI
   end
 
   def options
-    puts "What would you like to do?"
-    #1. List all meetups attended and details (oldest to newest)
-    Lubyfisbang::Meetup.print_all_meetups
-    #2. List all meetups attended and details (newsest to oldest)
-    Lubyfisbang::Meetup.order_by_date_new_to_old
-    #3. List details of a single attribute for all meetups
-    format_attribute_options
-    puts "Which attribute would you like to see deatils about?"
-    atribute = gets.chomp
-    Lubyfisbang::Meetup.list(attribute)
-    #4. List details about a meetup group
-    Lubyfisbang::GroupDetails.print_all_meetups
-    #5. List details about the meetup venue
-    Lubyfisbang::VenueDetails.print_all_meetups
-    #6. Find a meetup based on an attribute value.
-    Lubyfisbang::Meetup.find_meetup(attribute, value)
+    puts "What would you like to do? Enter the number of the option you want to initiate."
+    puts "1. List all meetups attended and details (oldest to newest)"
+    puts "2. List all meetups attended and details (newsest to oldest)"
+    puts "3. List details of a single attribute for all meetups"
+    puts "4. List details about a meetup group"
+    puts "5. List details about the meetup venue"
+    puts "6. Find a meetup based on an attribute value."
+    input = gets.chomp.to_i
+    case input
+      when 1
+        Lubyfisbang::Meetup.print_all_meetups
+      when 2
+        Lubyfisbang::Meetup.order_by_date_new_to_old
+      when 3
+        format_attribute_options
+        puts "Which attribute would you like to see deatils about?"
+        atribute = gets.chomp
+        Lubyfisbang::Meetup.list(attribute)
+      when 4
+        Lubyfisbang::GroupDetails.print_all_meetups
+      when 5
+        Lubyfisbang::VenueDetails.print_all_meetups
+      when 6
+        Lubyfisbang::Meetup.find_meetup(attribute, value)
+    end
   end
 
   def list_attribute_options(attributes)
